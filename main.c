3e347cc979faaf11c553a9bc070f6211abb84591
Victor Noriega <victornoriega7@gmail.com>
2018-05-15 14:57:59 -0700

    el ricky no tiene amigos

---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int* charArrayToIntArray(char* c);
+
+char* intArrayToCharArray(int *c, int);
+
+int main()
+{
+    char cadena[300];
+    scanf("%[^\n]s",cadena);
+    int *intArray = charArrayToIntArray(cadena);
+    printf("%s\n","Cadena convertida a numeros: ");
+    for(int i=0; i< 2*strlen(cadena); i++) printf("%u", intArray[i]);
+    char *charArray = intArrayToCharArray(intArray, strlen(cadena));
+    printf("%s\n","\nArreglo de numeros convertido a cadena original: ");
+    for(int i=0 ; i < strlen(cadena); i++) printf("%c", charArray[i]);
+    return 0;
+}
+
+/*
+    Name: charArrayToIntArray
+    Description: La siguiente funcion tiene como proposito  convertir un arreglo de caracteres en un arreglo de enteros.
+    Solo transforma las 26 letras del alfabeto ingles y el espacio. Cada una de estas letras ocupa dos espacios en el arreglo de enteros.
+    Haciendo que el tamano del arreglo de enteros sea el doble que el de caracteres. A cada letra se le asigna un numero con respecto a
+    su posicion en el alfabeto. De esta manera, a la letra A se le asigna 01.
+    Ejemplo: Cadena: Victor. Conversion: 22 09 03 20 15 18. En el arreglo de enteros se vera como 220903201518
+*/
+
+int* charArrayToIntArray(char* c){
+    int *array = malloc(2*strlen(c)*(sizeof(int)));
+    for(int i=0; i< strlen(c);i++){
+        c[i] = toupper(c[i]);
+        if((int)c[i] - 64 > 9){
+            if((int)c[i]== 127){
+                array[2*i] = 2;
+                array[2*i+1] = 7;
+            }else{
+                array[2*i] = ((int)c[i]-64)/10;
+                array[2*i +1] = ((int)c[i]-64)%10;
+            }
+        }else{
+            array[2*i] = 0;
+            array[2*i+1] = (int)c[i] - 64;
+        }
+    }
+    return array;
+}
+
+char* intArrayToCharArray(int *c, int lengthIntArray){
+    char *array = malloc(lengthIntArray * sizeof(char));
+
+    for(int i = 0; i < lengthIntArray; i++){
+        array[i] = (char)(64 + c[2*i]*10 + c[2*i+1]);
+    }
+    return array;
+}
